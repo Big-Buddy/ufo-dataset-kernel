fd2b314348135e05b15115f560e4eb410e0a8f92
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-03-20 20:14:31 -0400

    env.sh adjustment

--- env.sh
+++ env.sh
@@ -1,5 +1,4 @@
 module load spark
 module load python/3.5.1
 setenv PYTHONPATH $SPARK_HOME/python:$SPARK_HOME/python/build:$SPARK_HOME/python/lib/py4j-0.10.4-src.zip
-python -mpip install -U pip
-python -mpip install -U matplotlib
+pip install plotly
