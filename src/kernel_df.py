8885b0c330d41f16cb794fbac2a190f68e1ccd38
Alexandre Pelletier <pel_a@bednorz.encs.concordia.ca>
2018-04-04 16:34:01 -0400

    added specifications for each task

--- src/kernel_df.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import SparkSession
-from pyspark.sql import Row
-from pyspark.sql.functions import lit
-
-spark = SparkSession.builder.master("local").appName("lab1").getOrCreate()
--- src/kernel_rdd.py
+++ src/kernel_rdd.py
@@ -1,5 +1,21 @@
 import csv
+import sys
 from pyspark import SparkContext, SparkConf
 
 conf = SparkConf().setAppName("").setMaster("local")
 sc = SparkContext(conf=conf)
+
+data_file = sys.argv[1]
+
+###MAP REDUCE SPECS
+	#avg duration of sighting
+	#north vs south hemisphere
+	#population centers
+###FREQUENT PATTERNS
+	#words used in descriptions
+	#locations of sightings
+	#time of sightings (season, month, day/night)
+###CLUSTERING
+	#reliable sightings vs unreliable (look at possible features)
+###MAPS
+	#impose x-y coordinate map on world?!??!
