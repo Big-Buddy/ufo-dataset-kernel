4dded2f9b21133a25e68c15a248ef367082f8363
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-04-06 20:23:17 -0400

    cant use pip packages on concordia machines so removing for now

--- env.sh
+++ env.sh
@@ -1,4 +1,3 @@
 module load spark
 module load python/3.5.1
 setenv PYTHONPATH $SPARK_HOME/python:$SPARK_HOME/python/build:$SPARK_HOME/python/lib/py4j-0.10.4-src.zip
-pip install python-google-places
