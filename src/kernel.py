153bae01bddf31730c89622386915c7a86f740cc
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-04-07 15:56:49 -0400

    debugging avg_duration

--- src/kernel.py
+++ src/kernel.py
@@ -100,6 +100,8 @@ with open("./data/stopwords.txt", 'r') as f:
 
 ###READ
 lines = spark.read.text(data_file).rdd
+header = lines.first()
+lines = lines.filter(lambda x: x != header)
 parts = lines.map(lambda row: row.value.split(","))
 ufoRDD = parts.map(lambda x: Row(datetime=x[0], state=x[2], country=x[3], shape=x[4], duration=x[5], comment=x[7], latitude=x[9], longitude=x[10]))
 
